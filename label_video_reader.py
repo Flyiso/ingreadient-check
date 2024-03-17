6f9aec243de01f8828b5fb8083927a93a33089e6
Flyiso <dexterpetterssonspang@gmail.com>
2024-03-17 17:42:35 +0100

    edits- experimental stuff

--- label_video_reader.py
+++ label_video_reader.py
@@ -1,6 +1,7 @@
 import pytesseract as pt
 import cv2
 from create_panorama import CreatePanorama
+import numpy as np
 
 
 class ReadLabelVideo:
@@ -23,13 +24,13 @@ class ReadLabelVideo:
         self.pan_group = pan_group
         self.video_path = video_path
         self.start_video(frame_l, frame_s)
-        self.panorama = self.frame_manager.panorama_created
-        if save_img is True:
-            for id, pan in enumerate(self.panorama):
-                print(type(pan))
-                if pan is not None:
-                    print('saves!')
-                    cv2.imwrite(f'outputs/outputs_6/PANORAMA_{id}.png', pan)
+        #self.panorama = self.frame_manager.panorama_created
+        #if save_img is True:
+        #    for id, pan in enumerate(self.panorama):
+        #        print(type(pan))
+        #        if pan is not None:
+        #            print('saves!')
+        #            cv2.imwrite(f'outputs/outputs_6/PANORAMA_{id}.png', pan)
 
     def start_video(self, frame_l, frame_s):
         """
@@ -125,19 +126,88 @@ class CollectFrames:
         """
         concludes if text can be found in frame
         """
-        data = pt.image_to_data(frame, config='--oem 3 --psm 6',
-                                output_type='dict',
-                                lang='swe')
-        if len(data['text']) >= 20:
-            self.find_text_region(data, frame)
-            return len(data['text'])
-        return 0
+        frame_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_g = cv2.GaussianBlur(frame_g, (5, 5), 0)
+        lines = cv2.HoughLinesP(frame_g,
+                                rho = 1,
+                                theta = np.pi / 180,
+                                threshold = 100,
+                                lines = np.array([]),
+                                minLineLength = 50,
+                                maxLineGap = 5)
+        print(lines)
+        for line in lines:
+            for start_x, start_y, end_x, end_y in line:
+                cv2.line(frame, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
+        self.frame = frame
+        cv2.imwrite('outputs/shaped/hlines.png', frame)
+        #data = pt.image_to_data(frame_g, config='--oem 3 --psm 6',
+        #                        output_type='dict',
+        #                        lang='swe')
+        #if len(data['text']) >= 20:
+        #    self.find_text_region(data, frame)
+        #    return len(data['text'])
+        return 1
 
     def find_text_region(self, data, frame):
         """
         Crop out the section of the image where text is found.
         """
+        s_size = 100
+        section_width = int(frame.shape[1]/s_size)
+        section_height = int(frame.shape[0]/s_size)
+        long = []
+        lati = []
+        for n in range(1, s_size+1):
+            hig = [0, 0]
+            btm = [0, frame.shape[1]]
+            lft = [frame.shape[0], 0]
+            rgt = [0, 0]
+            for left, top in zip(data['left'], data['top']):
+                if (section_width*n)-section_width <= left and left <= n*section_width:
+                    if hig[1] < top:
+                        hig = [left, top]
+                    if btm[1] > top:
+                        btm = [left, top]
+                if (n*section_height)-section_height <= top and top <= section_height*n:
+                    if rgt[0] < left:
+                        rgt = [left, top]
+                    if lft[0] > left:
+                        lft = [left, top]
+            if btm[1] == frame.shape[1]:
+                btm[1] = 0
+            if lft[0] == frame.shape[0]:
+                lft[0] = 0
+            long.append([tuple(hig), tuple(btm)])
+            lati.append([tuple(lft), tuple(rgt)])
+        #vertices = upper + right + lower + left_
+        #vertices = set(vertices)
+        #vertices = list(vertices)
+
+        for ve in long:
+            if isinstance(ve[1], tuple) and isinstance(ve[0], tuple):
+                if ve[0] != (0, 0) and ve[1] != (0, 0):
+                    cv2.line(frame, ve[0], ve[1], (0, 255, 0), 2)
+        for ve in lati:
+            if isinstance(ve[1], tuple) and isinstance(ve[0], tuple):
+                if ve[0] != (0, 0) and ve[1] != (0, 0):
+                    cv2.line(frame, ve[0], ve[1], (255, 0, 255), 2)
+        for w, h in zip(data['left'], data['top']):
+            cv2.circle(frame, (w, h), 1, (0, 0, 255), 3)
+        cv2.imwrite('outputs/dotted.png', frame)
+
+
         self.frame = frame[min(data['left'][:1]):
                            max(data['left'][:1])+max(data['width'][:1]),
                            min(data['top'][:1]):
                            max(data['top'][1:])+max(data['height'][1:])]
+
+    def create_roi_mask(self, vertices, edges):
+        """
+        create roi mask for interesting section of video.
+        """
+        mask = np.zeros_like(edges)
+        mask_color = 255
+        polyg = cv2.fillPoly(mask, vertices, mask_color)
+        bit_mask = cv2.bitwise_and(edges, mask)
+        return bit_mask
--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def run_app(filepath):
     Run all components of application in order-
     filepath: path to video of label
     """
-    frame_l = 10
-    frame_s = 30
-    pan_group = 3
+    frame_l = 2
+    frame_s = 2
+    pan_group = 5
     save_img = False
 
     configs = ['--psm 1', '--psm 2', '--psm 3', '--psm 4', '--psm 5',
@@ -22,20 +22,21 @@ def run_app(filepath):
     label_data = ReadLabelVideo(filepath, save_img,
                                 frame_l, frame_s,
                                 pan_group)
-    for config in configs:
-        print('')
-        print(config)
-        print('')
-        try:
-            text_data = FindText(frames=label_data.panorama, config=config)
-            for item in text_data.text:
-                print(item)
-                print('...')
-        except FileNotFoundError:
-            print('DID NOT WORK')
-        except pt.TesseractError:
-            print('DID NOT WORK FOR ANOTHER REASON')
-        print('')
+    
+    #for config in configs:
+    #    print('')
+    #    print(config)
+    #    print('')
+    #    try:
+    #        text_data = FindText(frames=label_data.panorama, config=config)
+    #        for item in text_data.text:
+    #            print(item)
+    #            print('...')
+    #    except FileNotFoundError:
+    #        print('DID NOT WORK')
+    #    except pt.TesseractError:
+    #        print('DID NOT WORK FOR ANOTHER REASON')
+    #    print('')
 
 
 run_app('test_video.mp4')
