81b672c7b92f7fa804817ad3f88a42a4004c1360
Flyiso <dexterpetterssonspang@gmail.com>
2024-03-29 22:24:18 +0100

    update of roi-crop in image-display to crop and draw roi

--- image_display.py
+++ image_display.py
@@ -18,7 +18,8 @@ def crop_to_min_rectangle_with_lines(frame):
 
     # Threshold the grayscale image to obtain a binary mask of non-black pixels
     _, mask = cv2.threshold(gray, 30, 250, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+                                   cv2.CHAIN_APPROX_SIMPLE)
 
     contour_max = max(contours, key=cv2.contourArea)
     approx = []
@@ -29,5 +30,16 @@ def crop_to_min_rectangle_with_lines(frame):
         # Draw the approximated contour on the image
         approx.append(approx_c)
     largest_approximation = max(approx, key=cv2.contourArea)
+    frame = crop_outside_contour(frame, largest_approximation)
     cv2.drawContours(frame, [largest_approximation], -1, (0, 255, 0), 2)
     return frame
+
+
+def crop_outside_contour(frame, largest_approximation):
+    # Create a mask for the contour
+    contour_mask = np.zeros_like(cv2.cvtColor(frame,  cv2.COLOR_RGB2GRAY))
+    # Draw contours on the mask with a specific color (in BGR format)
+    cv2.drawContours(contour_mask, [largest_approximation], -1,
+                     (255), thickness=cv2.FILLED)
+    image = cv2.bitwise_and(frame, frame,  mask=contour_mask)
+    return image
