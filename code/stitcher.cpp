a74ae658aea2d5b724dd3d65694047b0ec0634d4
Flyiso <dexterpetterssonspang@gmail.com>
2024-05-24 09:44:05 +0200

    removed files

--- code/stitcher.cpp
+++
@@ -1,9 +0,0 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/stitching.hpp>
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
-#include <pybind11/stl.h>
-#include <vector>
-
-namespace py = pybind11;
-
