247f43277c7e0b2c8cc6cc91082bc1799ec773c1
Flyiso <dexterpetterssonspang@gmail.com>
2024-06-19 15:41:13 +0200

    method to adjust frame added.

--- code/depth_estimator.py
+++ code/depth_estimator.py
@@ -19,8 +19,7 @@ class DepthCorrection:
     method just correct by values?
     """
     def __init__(self, frame: np.ndarray) -> None:
-        frame_org = frame
-        frame_cv2 = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        frame_bgra = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
         alpha_channel = np.where(
             (frame[:, :, 0] == 0) &
             (frame[:, :, 1] == 0) &
@@ -28,9 +27,9 @@ class DepthCorrection:
             0,
             255
         )
-        frame_cv2[:, :, 3] = alpha_channel
-        cv2.imwrite('masked.png', frame_cv2)
-        frame = Image.fromarray(frame_cv2)
+        frame_bgra[:, :, 3] = alpha_channel
+        cv2.imwrite('masked.png', frame_bgra)
+        frame = Image.fromarray(frame_bgra)
         pipe = pipeline(task="depth-estimation",
                         model="LiheYoung/depth-anything-large-hf")
         depth = np.array(pipe(frame)["depth"])
@@ -51,17 +50,69 @@ class DepthCorrection:
         print(np.mean(depth[mask]))
         print(np.median(depth[mask]))
         print('....')
+        #warper = cv2.PyRotationWarper("cylindrical", 1.0)
+        #_, warped_image = warper.warp(src=frame_cv2,
+        #                              K=depth, R=depth,
+        #                              interp_mode=cv2.INTER_LINEAR,
+        #                              border_mode=cv2.BORDER_REFLECT)
 
         cv2.imwrite('depth_gs.png', depth)
-        self.frame = False
+        self.correct_image(frame=frame_bgra, depth_mask=depth)
 
     def _estimate_depth(self, frame, mask):
         pass
 
     def correct_image(self, frame: np.ndarray,
-                      mask: np.ndarray) -> np.ndarray:
+                      depth_mask: np.ndarray) -> np.ndarray:
         """
         use estimated size to correct
         the images shape and perspective
+        frame: BGRA image
+        depth_mask: GRAYSCALE image
+        mask: 
         """
-        pass
+        frame = frame  # bottle_image
+        depth_mask = cv2.normalize(depth_mask, None, 0, 255, cv2.NORM_MINMAX)
+
+        # Ensure the images are of the same size
+        assert depth_mask.shape == frame.shape[:2], '''
+                Depth image and bottle image must be of the same dimensions.'''
+        #print(frame[:, :, 3])
+        # Normalize depth image to [0, 1] range
+        #depth_normalized = cv2.normalize(depth_mask, None,
+        #                                 alpha=0, beta=1,
+        #                                 norm_type=cv2.NORM_MINMAX,
+        #                                 mask=frame[:, :, 3])
+        #depth_normalized = 1 - depth_normalized
+        #cv2.imwrite('depth_norm.png', depth_normalized)
+        #print(depth_normalized)
+        #input('')
+        cv2.imwrite('d_msk.png', depth_mask)
+        # Create a transformation map based on depth information
+        height, width = depth_mask.shape
+        map_x = np.zeros((height, width), np.float32)
+        map_y = np.zeros((height, width), np.float32)
+        max_displacement = width / 2
+        max_displacement_h = height / 2
+        for y in range(height):
+            print(depth_mask[y])
+            for x in range(width):
+                (width//2-x)  # distance to center width.
+                (height//2-y)  # distance to center height
+                (width//2)  # maximum width distance
+                (height//2)  # maximum distance height
+                # get displacement maps from middle distance & depth_mask?
+                # depth_mask increase values for push and pull?
+                # skip add all 0 to closest corner? or edge? or ignore them?
+                displacement = depth_mask[y, x] * max_displacement
+                displacement_h = depth_mask[y, x] * max_displacement_h
+                map_x[y, x] = x + displacement * x / width
+                map_y[y, x] = y + displacement_h * y / height
+        # Apply the transformation map to the whole image
+        flattened_image = cv2.remap(frame, map_x, map_y,
+                                    interpolation=cv2.INTER_LANCZOS4,
+                                    borderMode=cv2.BORDER_WRAP)  #,  # was cv2.INTER_LINEAR
+                                    #  borderMode=cv2.BORDER_TRANSPARENT)
+
+        cv2.imwrite('flat_img.png', flattened_image)
+        self.frame = flattened_image
