f78c0614a59f75e2d2e20f3cc9661237bd5e95b7
Flyiso <dexterpetterssonspang@gmail.com>
2024-06-20 23:23:20 +0200

    base/fist code for img flatterning added

--- code/depth_estimator.py
+++ code/depth_estimator.py
@@ -7,6 +7,7 @@ run this once or a few times? when?
 """
 import numpy as np
 import cv2
+import heapq
 from transformers import pipeline
 from PIL import Image
 
@@ -94,16 +95,23 @@ class DepthCorrection:
         map_y = np.zeros((height, width), np.float32)
         max_displacement = width / 2
         max_displacement_h = height / 2
+        x = 0
+        #for x in range(width):
+        #    self.get_map_row(depth_mask[x])
+        for y in range(height):
+            map_y = self.get_map_row([w_vals[y] for w_vals in depth_mask])
+            [map_y[n] for n in range(len(width))]
         for y in range(height):
             for x in range(width):
-                print(f'x: {depth_mask[x]}')
-                print(f'y: {[w_vals[y] for w_vals in depth_mask]}')
-                print('.....')
-                input('')
-                (width//2-x)  # distance to center width.
-                (height//2-y)  # distance to center height
-                (width//2)  # maximum width distanc
-                (height//2)  # maximum distance height
+                #print(f'x: {depth_mask[x]}')
+                #print(f'y: {[w_vals[y] for w_vals in depth_mask]}')
+                #lft = sum(width[:width//2]) # sum of depth.
+                #rght = sum(width[width//2:])
+                #(width//2-x)  # distance to center width.
+                #(height//2-y)  # distance to center height
+                #(width//2)  # maximum width distance
+                #(height//2)  # maximum distance height
+                # add tol until max 225, then spread all to width//2
                 # get displacement maps from middle distance & depth_mask?
                 # depth_mask increase values for push and pull?
                 # skip add all 0 to closest corner? or edge? or ignore them?
@@ -115,8 +123,12 @@ class DepthCorrection:
                 # just fill a list?
                 displacement = depth_mask[y, x] * max_displacement
                 displacement_h = depth_mask[y, x] * max_displacement_h
-                map_x[y, x] = x + displacement * x / width
-                map_y[y, x] = y + displacement_h * y / height
+                map_x[y, x] = x - displacement * x / width
+                print(f'x: {x}, y: {y} --- {map_x[y, x]}')
+                map_y[y, x] = y - displacement_h * y / height
+                print(f'x: {x}, y: {y} --- {map_y[y, x]}')
+                print('............')
+            #input('.....')
         # Apply the transformation map to the whole image
         flattened_image = cv2.remap(frame, map_x, map_y,
                                     interpolation=cv2.INTER_LANCZOS4,
@@ -125,3 +137,55 @@ class DepthCorrection:
 
         cv2.imwrite('flat_img.png', flattened_image)
         self.frame = flattened_image
+
+    def get_map_row(self, pixel_row):
+        """
+        uses self.distribute to get values for map.
+        """
+        map_a = self.distribute(pixel_row[:(len(pixel_row))//2])
+        map_b = self.distribute(pixel_row[(len(pixel_row)//2)::-1])[-1]
+        if len(map_a+map_b) == len(pixel_row):
+            fin_map = map_a+map_b
+            return fin_map
+        else:
+            print(f'img_section_original: {len(pixel_row)}')
+            print(f'img_section_new_size: {len(map_a+map_b)}')
+
+    def distribute(self, pixels):
+        """
+        pixels: depth pixels of half of the height
+                or width currently working with
+        """
+        len(pixels)  # how many spaces to fill.
+
+        pxl_groups = []
+        curr_group = {'pixel_ids': [], 'pxl_sum': 0, 'pxl_space': 0}
+        for pxl_id, pxl in enumerate(pixels):
+            if curr_group['pxl_sum'] + pxl <= 225:
+                curr_group['pxl_sum'] += pxl
+                curr_group['pixel_ids'].append(pxl_id)
+            else:
+                pxl_groups.append(curr_group)
+                curr_group = {'pixel_ids': pxl_id, 'pxl_sum': pxl, 'spaces': 0}
+
+        pxl_space = (len(pixels)) // (len(pxl_groups))
+        pxl_remain = (len(pixels)) % (len(pxl_groups))
+        remain_ids = heapq.nlargest(pxl_remain,
+                                    range(len(pxl_groups)),
+                                    key=lambda i: pxl_groups[i]['pxl_sum'])
+        return_map = []
+        used_ids = 0
+        #  each g_id = one new value in return map.
+        for g_id, group in enumerate(pxl_groups):
+            total = pxl_space
+            if g_id in remain_ids:
+                total += 1
+            to_add = total/(len(group['pixel_ids']))
+            for n in range(1, len(group['pixel_ids']+1)):
+                return_map.append(round(used_ids+(to_add*n)))
+            used_ids += total
+
+        if len(return_map) == len(pixels):
+            return return_map
+        print(f'img_section_original: {len(pixels)}')
+        print(f'img_section_new_size: {len(return_map)}')
