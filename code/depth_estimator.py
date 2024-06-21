69a46e9c6bf3cefc787b34b8b1c91f5503725805
Flyiso <dexterpetterssonspang@gmail.com>
2024-06-21 19:43:20 +0200

    re-maping by depth functional. added notes for edits

--- code/depth_estimator.py
+++ code/depth_estimator.py
@@ -51,11 +51,6 @@ class DepthCorrection:
         print(np.mean(depth[mask]))
         print(np.median(depth[mask]))
         print('....')
-        #warper = cv2.PyRotationWarper("cylindrical", 1.0)
-        #_, warped_image = warper.warp(src=frame_cv2,
-        #                              K=depth, R=depth,
-        #                              interp_mode=cv2.INTER_LINEAR,
-        #                              border_mode=cv2.BORDER_REFLECT)
 
         cv2.imwrite('depth_gs.png', depth)
         self.correct_image(frame=frame_bgra, depth_mask=depth)
@@ -70,32 +65,15 @@ class DepthCorrection:
         the images shape and perspective
         frame: BGRA image
         depth_mask: GRAYSCALE image
-        mask: 
         """
         frame = frame  # bottle_image
         depth_mask = cv2.normalize(depth_mask, None, 0, 255, cv2.NORM_MINMAX)
 
-        # Ensure the images are of the same size
         assert depth_mask.shape == frame.shape[:2], '''
                 Depth image and bottle image must be of the same dimensions.'''
-        #print(frame[:, :, 3])
-        # Normalize depth image to [0, 1] range
-        #depth_normalized = cv2.normalize(depth_mask, None,
-        #                                 alpha=0, beta=1,
-        #                                 norm_type=cv2.NORM_MINMAX,
-        #                                 mask=frame[:, :, 3])
-        #depth_normalized = 1 - depth_normalized
-        #cv2.imwrite('depth_norm.png', depth_normalized)
-        #print(depth_normalized)
-        #input('')
+
         cv2.imwrite('d_msk.png', depth_mask)
-        # Create a transformation map based on depth information
         height, width = depth_mask.shape
-        map_x = np.zeros((height, width), np.float32)
-        map_y = np.zeros((height, width), np.float32)
-        max_displacement = width / 2
-        max_displacement_h = height / 2
-        x = 0
         map_a = np.array(
             [self.get_map_row(depth_mask[y])
              for y in range(height)]).astype(np.float32)
@@ -103,47 +81,26 @@ class DepthCorrection:
             [self.get_map_row([w_vals[x]
              for w_vals in depth_mask])
              for x in range(0, width)])).astype(np.float32)
-
+        map_a = cv2.normalize(map_a, None, 0, 255, cv2.NORM_MINMAX)
+        map_b = cv2.normalize(map_b, None, 255, 0, cv2.NORM_MINMAX)
+        map_a = cv2.blur(map_a, (95, 95))
+        map_b = cv2.blur(map_b, (95, 95))
+        #map_a = cv2.medianBlur(map_a, 5)
+        #map_b = cv2.medianBlur(map_b, 5)
+        #map_a = cv2.GaussianBlur(map_a, (95, 95), 100)
+        #map_b = cv2.GaussianBlur(map_b, (95, 95), 100)
+        #map_a = cv2.bilateralFilter(map_a, 9, 75, 75)
+        #map_b = cv2.bilateralFilter(map_b, 9, 75, 75)
+        #map_a = cv2.bitwise_xor(map_a)
+        #map_b = cv2.bitwise_xor(map_b)
         cv2.imwrite('map_a.png', map_a)
         cv2.imwrite('map_b.png', map_b)
+        map_a = cv2.normalize(map_a, None, 0, width, cv2.NORM_MINMAX)
+        map_b = cv2.normalize(map_b, None, 0, height, cv2.NORM_MINMAX)
 
-
-        for y in range(height):
-            for x in range(width):
-                #print(f'x: {depth_mask[x]}')
-                #print(f'y: {[w_vals[y] for w_vals in depth_mask]}')
-                #lft = sum(width[:width//2]) # sum of depth.
-                #rght = sum(width[width//2:])
-                #(width//2-x)  # distance to center width.
-                #(height//2-y)  # distance to center height
-                #(width//2)  # maximum width distance
-                #(height//2)  # maximum distance height
-                # add tol until max 225, then spread all to width//2
-                # get displacement maps from middle distance & depth_mask?
-                # depth_mask increase values for push and pull?
-                # skip add all 0 to closest corner? or edge? or ignore them?
-                # use some distribution to get how much to adjust?
-                # sum limit of values to add close to each other? lots of 0
-                # does not change anything, but high values cant be close? 
-                # some neighbor limit? (max 225 in 3*3 ngh? or similar?)
-                # go with max then? or use mean(median better?)
-                # just fill a list?
-                displacement = depth_mask[y, x] * max_displacement
-                displacement_h = depth_mask[y, x] * max_displacement_h
-                map_x[y, x] = x - displacement * x / width
-                print(f'x: {x}, y: {y} --- {map_x[y, x]}')
-                map_y[y, x] = y - displacement_h * y / height
-                print(f'x: {x}, y: {y} --- {map_y[y, x]}')
-                print('............')
-            #input('.....')
-        # Apply the transformation map to the whole image
-        print(type(map_x))
-        print(type(map_y))
         flattened_image = cv2.remap(frame, map_a, map_b,
                                     interpolation=cv2.INTER_LANCZOS4,
-                                    borderMode=cv2.BORDER_WRAP)  #,  # was cv2.INTER_LINEAR
-                                    #  borderMode=cv2.BORDER_TRANSPARENT)
-
+                                    borderMode=cv2.BORDER_WRAP)
         cv2.imwrite('flat_img.png', flattened_image)
         self.frame = flattened_image
 
@@ -151,12 +108,19 @@ class DepthCorrection:
         """
         uses self.distribute to get values for map.
         """
-        map_a = self.distribute(pixel_row
-                                [:((len(pixel_row))//2)])
-        map_b = self.distribute(pixel_row[((len(pixel_row))//2):][::-1])[::-1]
-        if len(map_a+map_b) == len(pixel_row):
-            fin_map = map_a+map_b
-            return fin_map
+        #map_a = self.distribute(pixel_row
+        #                        [:((len(pixel_row))//2)])
+        #map_b = self.distribute(pixel_row[(len(pixel_row)//2)::])
+        #print('......')
+        #print(pixel_row)
+        #print(map_a)
+        #print(map_b)
+        #if len(map_a+map_b) == len(pixel_row):
+        #    fin_map = map_b[::-1]+map_a
+        #    return fin_map
+        map_row = self.distribute(pixel_row)
+        if len(map_row) == len(pixel_row):
+            return map_row[::]
         else:
             print(len(pixel_row)//2)
             print(len(pixel_row) % 2)
@@ -170,38 +134,42 @@ class DepthCorrection:
                 or width currently working with
         """
         pxl_groups = []
-        curr_group = {'pixel_ids': [], 'pxl_sum': 0}
+        curr_group = {'pixel_ids': [], 'pxl_sum': []}
         for pxl_id, pxl in enumerate(pixels):
-            if curr_group['pxl_sum'] + pxl <= 255:
-                curr_group['pxl_sum'] += pxl
+            if sum(curr_group['pxl_sum']) + pxl <= 255:
+                curr_group['pxl_sum'].append(pxl)
                 curr_group['pixel_ids'].append(pxl_id)
             else:
                 pxl_groups.append(curr_group)
-                curr_group = {'pixel_ids': [pxl_id], 'pxl_sum': pxl}
+                curr_group = {'pixel_ids': [pxl_id], 'pxl_sum': [pxl]}
         if len(curr_group['pixel_ids']) != 0:
             pxl_groups.append(curr_group)
 
-        pxl_space = (len(pixels)) // (len(pxl_groups))
+        pxl_space = (len(pixels)) / (len(pxl_groups))
         pxl_remain = (len(pixels)) % (len(pxl_groups))
         remain_ids = heapq.nlargest(pxl_remain,
                                     range(len(pxl_groups)),
-                                    key=lambda i: pxl_groups[i]['pxl_sum'])
+                                    key=lambda i: sum(pxl_groups[i]['pxl_sum']))
         return_map = []
         used_ids = 0
+        size = []
         #  each g_id = one new value in return map.
         for g_id, group in enumerate(pxl_groups):
+            size.append(len(group['pixel_ids']))
             total = pxl_space
             if g_id in remain_ids:
                 total += 1
             to_add = total/(len(group['pixel_ids']))
+            # HERE size/color of eack pixel should matter- w low value pixels should be bunched together
             for n in range(1, len(group['pixel_ids'])+1):
-                return_map.append(round(used_ids+(to_add*n)))
+                return_map.append(round((used_ids+(to_add*n))))
             used_ids += total
+        print(size)
+        print('....')
 
         if len(return_map) == len(pixels):
+            return_map.sort()
             return return_map
 
-        print(pixels)
-        print(return_map)
         print(f'img_section_original: {len(pixels)}')
         print(f'img_section_new_size: {len(return_map)}')
