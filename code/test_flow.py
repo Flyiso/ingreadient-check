960c3f5a5a998c7e1867471c24ea44a7be6d6b63
Flyiso <dexterpetterssonspang@gmail.com>
2024-09-02 14:43:56 +0200

    additional file for test of how to build final version of program

---
+++ code/test_flow.py
@@ -0,0 +1,30 @@
+"""
+This is temporary file to test/find the best way to run the
+program, using the sections of the original code with the
+best performance. This will be deleted at a later stage, but
+might be used as guidelines when designing the final version
+of the label reader.
+"""
+
+
+class VideoFlow:
+    """
+    This class manages the flow of the frames.
+    """
+    pass
+
+
+class ManageImage:
+    """
+    This class extract roi and transforms the image
+    to make it suitable for stitching.
+    """
+    pass
+
+
+class ManageStitcher:
+    """
+    This class manages the stitcher and makes ensures
+    quality and performance
+    """
+    pass
