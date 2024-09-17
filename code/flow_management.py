b05eaf44fcbd71f08cc68d76871140d86104255d
Flyiso <dexterpetterssonspang@gmail.com>
2024-09-17 16:59:09 +0200

    updates in frame flow

--- code/flow_management.py
+++ code/flow_management.py
@@ -5,13 +5,40 @@ class RecordLabel:
     """
     Class to manage flow, selection and storage of video frames
     """
-    def __init__(self):
+    def __init__(self, video_path: str):
+        # Update path to also allow live recording
+        self.video_path = video_path
+        self.frame_memory = []
+        self.merge_interval = 50
+        self.start_video()
+
         pass
         # find first 'good enough' frame
         # save frames in interval of x.
         # try to merge in last of saved
         # frames to panorama(or first frame)
-        # bactrac OR remove/restart frame storage
+        # backtrack OR remove/restart frame storage
         # possibly adjust frame distance.
         # from last successfull merge.
         # return final image
+
+    def start_video(self):
+        """
+        loop through all frames in video.
+        """
+        self.capture = cv2.VideoCapture(self.video_path)
+        while self.capture.isOpened():
+            ret, frame = self.capture.read()
+            if not ret or cv2.waitKey(25) & 0xFF == 27:
+                break
+            self.frame_memory.append(frame)
+            if len(self.frame_memory) >= self.merge_interval:
+                self.try_frame()
+        self.capture.release()
+        cv2.destroyAllWindows()
+
+    def try_frame(self):
+        """
+        Try to merge frame saved in self.frame_memory, looped backwards.
+        """
+        pass
