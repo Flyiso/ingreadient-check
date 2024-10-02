00d5a92e88f73d3d378d601a78abbc39d833287f
dex <dex@core>
2024-10-02 13:14:22 +0200

    base for pre-proceesing added

--- code/frame_preprocessing.py
+++ code/frame_preprocessing.py
@@ -1,3 +1,25 @@
 """
 All options for frame pre-process before merge attempt.
+TODO:
+#flatten image
+#remove artifacs/inpaint
+#enhance, increase contrasts
+#enhance-with clahe
+#segment/extract roi - text detected
+#segment/extract roi - DINO & SAM
 """
+import numpy as np
+from abc import ABC, abstractmethod
+from flow_management import RecordLabel
+
+
+class PreProcess(ABC):
+    """
+    Classes for frame pre-proceesing
+    """
+    @abstractmethod
+    def process_frame(self, frame: np.ndarray) -> np.ndarray:
+        """
+        return frame pre-proceesed
+        """
+
